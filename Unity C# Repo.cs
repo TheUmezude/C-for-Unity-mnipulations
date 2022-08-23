71c7c960fdb1a4c2f978caf7ed290e0b85f89300
olama68 <100791612+olama68@users.noreply.github.com>
2022-08-23 10:34:52 -0400

    Update Unity C# Repo.cs

--- Unity C# Repo.cs
+++ Unity C# Repo.cs
@@ -250,3 +250,49 @@ public class ChangeScene : MonoBehaviour
     }
 }
 //**********************************************************************************************************************************************************
+//IOT Turn on and turn off. with virtual button 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Networking;
+
+public class First : MonoBehaviour
+{
+    public VirtualButtonBehaviour Vb_on;
+    public VirtualButtonBehaviour Vb_off;
+    public string url_on;
+    public string url_off;
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+        }
+    }
+
+    void Start()
+    {
+        Vb_on.RegisterOnButtonPressed(OnButtonPressed_on);
+
+        Vb_off.RegisterOnButtonPressed(OnButtonPressed_off);
+       
+    }
+
+
+    public void OnButtonPressed_on(VirtualButtonBehaviour Vb_on)
+    {
+        StartCoroutine(GetRequest(url_on));
+        Debug.Log("LED IS ON");
+    }
+
+    public void OnButtonPressed_off(VirtualButtonBehaviour Vb_off)
+    {
+        StartCoroutine(GetRequest(url_off));
+        Debug.Log("LED IS OFF");
+    }
+
+}
