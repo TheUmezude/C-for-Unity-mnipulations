bc74896bef6b8f9873b468607d4bca45639c2a0f
olama68 <100791612+olama68@users.noreply.github.com>
2022-08-22 23:46:06 -0400

    virtual Button

--- Unity C# Repo.cs
+++ Unity C# Repo.cs
@@ -250,3 +250,36 @@ public class ChangeScene : MonoBehaviour
     }
 }
 //**********************************************************************************************************************************************************
+/ / virtual button
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+ 
+public class vb_anim : MonoBehaviour
+{
+ 
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("LacieBtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+ 
+        cubeAni.GetComponent<Animator>();
+    }
+ 
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("cube_animation");
+        Debug.Log("Button pressed");
+    }
+ 
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Button released");
+    }
+}
